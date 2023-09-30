9658803aa37e33f9f2e22fc48d00ac9730763e31
XpArKeR <xparker@gmx.net>
2023-09-30 07:45:12 +0200

    Added Scene shells

---
+++ Code/ldjam54/Assets/Scripts/Constants/SceneNames.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assets.Scripts.Constants
+{
+    public class SceneNames
+    {
+        public const String Credits = "CreditsScene";
+        public const String GameOver = "GameOverScene";
+        public const String MainMenu = "MainMenuScene";
+        public const String Options = "OptionsScene";
+        public const String Space = "SpaceScene";
+    }
+}
