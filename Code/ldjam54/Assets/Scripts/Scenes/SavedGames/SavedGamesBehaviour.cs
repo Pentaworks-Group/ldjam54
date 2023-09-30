76a8128ef70e993f36d5319742ed7e41f44a6c21
XpArKeR <xparker@gmx.net>
2023-09-30 16:42:50 +0200

    Added SavedGamesScene

---
+++ Code/ldjam54/Assets/Scripts/Scenes/SavedGames/SavedGamesBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Assets.Scripts.Scenes.Menues;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Scenes.Credits
+{
+    public class SavedGamesBehaviour : BaseMenuBehaviour
+    {
+    }
+}
